74df5592002f2595327573e577bf511e5cd798af
Torning, Fraser <Fraser_Torning@comcast.com>
2018-05-25 17:05:51 -0400

    forgot to add finished portfolio and contact pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 $( document ).ready(function() {
     console.log("Ayyyoooo");
    
-    $("#btn-submit").click( function() {
+    $("#submit").click( function() {
         var cname = $("#name").val();
         var cemail = $("#email").val();
         var cmsg = $("#message").val();
-        alert("Can't send an email without ajax\n\n" + "\nName = " + cname + "\nEmail = " + cemail + "\nMessage = " + cmsg);
+        alert("Can't send an email\n\n" + "\nName = " + cname + "\nEmail = " + cemail + "\nMessage = " + cmsg);
         $("#name").val("");
         $("#email").val("");
         $("#message").val("");
